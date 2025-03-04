405018ff78cd374b000bfe30791b80e67ed1cd47
Tomas Apodaca <546665+tmaybe@users.noreply.github.com>
2025-03-04 11:33:36 -0800

    saving cards summary

--- src/summarize.ts
+++ src/summarize.ts
@@ -1,9 +1,10 @@
+import * as trackerRadarDomainCache from "../packages/blacklight-lambda/src-api-lambda/domain-cache";
+import ObjectsToCsv from "objects-to-csv";
 import fs from "fs";
 import path from "path";
-import ObjectsToCsv from "objects-to-csv";
+import reportedCardsData from "../packages/blacklight-lambda/src-api-lambda/reported-cards";
 import { cardGroups } from '../packages/blacklight-lambda/src-cards-writer/index';
-import * as trackerRadarDomainCache from '../packages/blacklight-lambda/src-api-lambda/domain-cache';
-import * as reportedCardsData from '../packages/blacklight-lambda/src-api-lambda/reported-cards';
+import { stripHtml } from "string-strip-html";
 
 interface QuerySummary {
   url: string;
@@ -19,6 +20,25 @@ interface QuerySummary {
   key_logging_events: string;
   session_recorder_events: string;
   third_party_trackers_count: number;
+}
+
+interface CardsSummary {
+  url: string;
+  ad_trackers_number: number;
+  ad_trackers_owners: string; // list of tracker companies
+  ad_trackers_statement: string; // e.g. 'this is more than the average of...'
+  cookies_number: number;
+  cookies_owners: string; // list of cookie companies
+  cookies_statement: string; // e.g. 'this is more than the average of...'
+  canvas_fingerprinting_found: string;
+  canvas_fingerprinting_owners: string; // list of script owners
+  session_recording_found: string;
+  session_recording_owners: string; // list of script owners
+  key_logging_found: string;
+  key_logging_owners: string; // list of script owners
+  pixel_found: string;
+  google_remarketing_found: string;
+  ad_tech_companies: string; // list of ad-tech companies interacted with
 }
 
 // get the current date & time as an ISO string
@@ -130,7 +150,8 @@ const summarizeCanvasFingerprinters = (canvasPrints: any, fontPrints: any) => {
 // Read all JSON files and process each listing
 const processDirectory = async (directory: string) => {
   const files = fs.readdirSync(directory);
-  const summaries: QuerySummary[] = [];
+  const querySummaries: QuerySummary[] = [];
+  const cardsSummaries: CardsSummary[] = [];
 
   for (const entry of files) {
     const entryPath = path.join(directory, entry);
@@ -143,7 +164,7 @@ const processDirectory = async (directory: string) => {
         try {
           const inspection = JSON.parse(fs.readFileSync(inspectionPath, "utf8"));
 
-          const summary: QuerySummary = {
+          const querySummary: QuerySummary = {
             url: inspection.uri_ins,
             behavior_event_listeners: Object.keys(inspection.reports.behaviour_event_listeners).join(", "),
             canvas_fingerprint_data_url_count: canvasFingerprintDataUrlCount(inspection.reports.canvas_fingerprinters),
@@ -158,7 +179,7 @@ const processDirectory = async (directory: string) => {
             session_recorder_events: sessionRecorderEvents(inspection.reports.session_recorders),
             third_party_trackers_count: inspection.reports.third_party_trackers.length,
           }
-          summaries.push(summary);
+          querySummaries.push(querySummary);
 
           // summarize contents of inspection.json
           console.log("------------------");
@@ -186,10 +207,40 @@ const processDirectory = async (directory: string) => {
           console.log(`> ${inspection.reports.third_party_trackers.length} third party tracker events!`);
           console.log("------------------");
 
-          const cardsData: any[] = reportedCardsData.default;
+          const cardsData: any[] = reportedCardsData;
           const domainCache: any = trackerRadarDomainCache;
           const { groups } = await cardGroups(inspection, inspectionPath, domainCache, cardsData);
-          console.log('groups is: ', JSON.stringify(groups));
+
+          const inspectionResult: any = groups.find((item: any) => item.title === 'Blacklight Inspection Result');
+          const adTrackersCard: any = inspectionResult.cards.find((item: any) => item.cardType === 'ddg_join_ads');
+          const cookiesCard: any = inspectionResult.cards.find((item: any) => item.cardType === 'cookies');
+          const fingerprintingCard: any = inspectionResult.cards.find((item: any) => item.cardType === 'canvas_fingerprinters');
+          const sessionRecordersCard: any = inspectionResult.cards.find((item: any) => item.cardType === 'session_recorders');
+          const keyLoggingCard: any = inspectionResult.cards.find((item: any) => item.cardType === 'key_logging');
+          const pixelCard: any = inspectionResult.cards.find((item: any) => item.cardType === 'fb_pixel_events');
+          const analyticsCard: any = inspectionResult.cards.find((item: any) => item.cardType === 'ga');
+
+          const someAdTechCompanies: any = groups.find((item) => item.title === 'Some of the ad-tech companies this website interacted with:');
+
+          const cardsSummary: CardsSummary = {
+            url: inspection.uri_ins,
+            ad_trackers_number: adTrackersCard?.bigNumber || 0,
+            ad_trackers_owners: adTrackersCard?.domainData?.owners?.join(", ") || "",
+            ad_trackers_statement: stripHtml(adTrackersCard?.onAvgStatement || "").result,
+            cookies_number: cookiesCard?.bigNumber || 0,
+            cookies_owners: cookiesCard?.domainData?.owners?.join(", ") || "None",
+            cookies_statement: stripHtml(cookiesCard?.onAvgStatement || "").result,
+            canvas_fingerprinting_found: fingerprintingCard?.testEventsFound ? "true" : "false",
+            canvas_fingerprinting_owners: fingerprintingCard?.domainData?.owners?.join(", ") || "",
+            session_recording_found: sessionRecordersCard?.testEventsFound ? "true" : "false",
+            session_recording_owners: sessionRecordersCard?.domainData?.owners?.join(", ") || "",
+            key_logging_found: keyLoggingCard?.testEventsFound ? "true" : "false",
+            key_logging_owners: keyLoggingCard?.domainData?.owners?.join(", ") || "",
+            pixel_found: pixelCard?.testEventsFound ? "true" : "false",
+            google_remarketing_found: analyticsCard?.testEventsFound ? "true" : "false",
+            ad_tech_companies: someAdTechCompanies?.cards?.map((item: any) => item.title)?.join(", ") || "",
+          }
+          cardsSummaries.push(cardsSummary);
 
         } catch (err: any) {
           console.log("error processing inspection!", err);
@@ -200,12 +251,16 @@ const processDirectory = async (directory: string) => {
     }
   }
 
+  // write to csvs
+  const querySummaryPath: string = `./${timestamp()}-query-summary.csv`;
+  const queryCSV = new ObjectsToCsv(querySummaries);
+  await queryCSV.toDisk(querySummaryPath);
+  console.log(`> query summary written to ${querySummaryPath}`);
 
-  // write to csv
-  const summaryPath: string = `./${timestamp()}-summary.csv`;
-  const csv = new ObjectsToCsv(summaries);
-  await csv.toDisk(summaryPath);
-  console.log(`> summary written to ${summaryPath}`);
+  const cardsSummaryPath: string = `./${timestamp()}-cards-summary.csv`;
+  const cardsCSV = new ObjectsToCsv(cardsSummaries);
+  await cardsCSV.toDisk(cardsSummaryPath);
+  console.log(`> cards summary written to ${cardsSummaryPath}`);
 }
 
 processDirectory("./testing");
